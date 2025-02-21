6cfb95e6d968f5ed511c87000827bf4aed437b69
Aviral Singh <aviralpratapsingh2002@gmail.com>
2025-02-21 18:19:17 +0530

    Post and routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import connectMongoDB from "./db/connectMongodb.js";
 import {v2 as cloudinary} from "cloudinary";
+import postRoutes from "./routes/post.route.js";
 dotenv.config();
 cloudinary.config({
 	cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
@@ -24,6 +25,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts",postRoutes);
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 	connectMongoDB();
