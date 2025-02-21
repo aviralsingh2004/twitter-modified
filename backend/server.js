e6fd48b4136f0602e96564f3298f1f7bcc8fbc89
Aviral Singh <aviralpratapsingh2002@gmail.com>
2025-02-21 14:18:53 +0530

    Third trail

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import connectMongoDB from "./db/connectMongodb.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
+});
