ffeedc2b2067c6cb1f156d4b6768133f59c02887
Aviral Singh <aviralpratapsingh2002@gmail.com>
2025-02-22 19:54:39 +0530

    Auth functionality is connected to frontend from backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ cloudinary.config({
 	cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
 	api_key:process.env.CLOUDINARY_API_KEY,
 	api_secret:process.env.CLOUDINARY_API_SECRET,
-})
+});
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
